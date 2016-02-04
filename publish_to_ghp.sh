b3af2cd2fb7ae553cb76186a3a25dd2306bf47ef
Lukas Woodtli (Travis CI) <woodtli.lukas@gmail.com>
2016-02-04 16:09:35 +0100

    Fix 3

--- publish_to_ghp.sh
+++ publish_to_ghp.sh
@@ -9,7 +9,7 @@ git config user.email "woodtli.lukas@gmail.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-git add ./FreeRTOS/doxy
+git add -f ./FreeRTOS/doxy
 git commit -m "Deploy to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote

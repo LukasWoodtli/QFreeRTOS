70de57d96dba118bb67be2fad7a521e7c530444a
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-06 15:10:06 +0100

    Fix installing qt on travis

--- travis_installs_linux.sh
+++ travis_installs_linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt-get -qq update
+#sudo apt-get -qq update
 #sudo apt-get -qq install g++-4.8 libc6-i386 qt54tools
 sudo apt-get -q install build-essential
 sudo apt-get -q install qt5-default

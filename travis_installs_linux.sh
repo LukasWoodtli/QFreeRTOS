7abe2d191df07941bdf2854bed3cb0fc42d6efc8
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-06 15:05:20 +0100

    Try to intall qt 5 on travis

--- travis_installs_linux.sh
+++ travis_installs_linux.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-lsb_release -a
-sudo apt-add-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-add-repository -y ppa:beineri/opt-qt542
 sudo apt-get -qq update
-sudo apt-get -qq install g++-4.8 libc6-i386 qt54tools
-export CXX="g++-4.8"
-export CC="gcc-4.8"
+#sudo apt-get -qq install g++-4.8 libc6-i386 qt54tools
+sudo apt-get -q install build-essential
+sudo apt-get -q install qt5-default

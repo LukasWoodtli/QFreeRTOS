ecee06f8d431fc83e192823fffa5f961c193ad3f
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-07 14:59:12 +0100

    Disable installation of doxgen on travis
    For now the documentation is not build

--- travis_installs_linux.sh
+++ travis_installs_linux.sh
@@ -3,7 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-sudo apt-get -y install graphviz doxygen mscgen
+#sudo apt-get -y install graphviz doxygen mscgen
 sudo apt-get -y install qtbase5-dev
 sudo apt-get -y install qt5-default qttools5-dev-tools
 sudo apt-get -q install build-essential

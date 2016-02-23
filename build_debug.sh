83803d27cb60a2dd61b4c9bf58f4d704bc7763c5
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-02-23 01:01:28 +0100

    Improve and add scripts for building on travis

---
+++ build_debug.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd FreeRTOS
+cmake -G "Unix Makefiles"
+make
--- travis_installs_linux.sh
+++ travis_installs_linux.sh
@@ -6,4 +6,5 @@ sudo apt-get update -qq
 #sudo apt-get -y install graphviz doxygen mscgen
 sudo apt-get -y install qtbase5-dev
 sudo apt-get -y install qt5-default qttools5-dev-tools
+sudo apt-get -y install cmake
 sudo apt-get -q install build-essential
